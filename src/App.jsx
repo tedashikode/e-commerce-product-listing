5e4987bd24b08d072a7977d70bab371f6c3aa599
tedashikode <theodoremawuli77@gmail.com>
2023-06-12 17:03:07 +0000

    setup the project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <div className="bg-green-950 text-white h-full w-full">
+        Let's get this show on the road
+      </div>
+    </>
+  )
+}
+
+
